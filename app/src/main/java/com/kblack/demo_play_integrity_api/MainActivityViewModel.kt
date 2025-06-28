b7aa35a44a9cf69c79e5be2900ca03c6e961560d
Kblack (Solo dev) <65351458+khoa083@users.noreply.github.com>
2025-06-28 22:45:12 +0700

    refactor

--- app/src/main/java/com/kblack/demo_play_integrity_api/MainActivityViewModel.kt
+++ app/src/main/java/com/kblack/demo_play_integrity_api/MainActivityViewModel.kt
@@ -1,8 +1,6 @@
 package com.kblack.demo_play_integrity_api
 
 import android.content.Context
-import android.util.Base64
-import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
@@ -15,24 +13,15 @@ import com.google.android.play.core.integrity.StandardIntegrityManager.PrepareIn
 import com.google.android.play.core.integrity.StandardIntegrityManager.StandardIntegrityToken
 import com.google.android.play.core.integrity.StandardIntegrityManager.StandardIntegrityTokenProvider
 import com.google.android.play.core.integrity.StandardIntegrityManager.StandardIntegrityTokenRequest
-import com.google.gson.Gson
 import com.kblack.base.BaseRepository
 import com.kblack.base.BaseViewModel
 import com.kblack.base.utils.DataResult
 import com.kblack.demo_play_integrity_api.model.PIAResponse
 import com.kblack.demo_play_integrity_api.repository.Repository
-import com.kblack.demo_play_integrity_api.utils.Constant
 import com.kblack.demo_play_integrity_api.utils.Constant.CLOUD_PROJECT_NUMBER
 import com.kblack.demo_play_integrity_api.utils.Utils.Companion.getRequestHashLocal
+import com.kblack.demo_play_integrity_api.utils.Utils.Companion.sendTokenToLocal
 import kotlinx.coroutines.launch
-import org.jose4j.jwe.JsonWebEncryption
-import org.jose4j.jws.JsonWebSignature
-import org.jose4j.jwx.JsonWebStructure
-import java.security.KeyFactory
-import java.security.PublicKey
-import java.security.spec.X509EncodedKeySpec
-import javax.crypto.SecretKey
-import javax.crypto.spec.SecretKeySpec
 
 class MainActivityViewModel(
     private val repository: Repository = Repository()
@@ -126,66 +115,12 @@ class MainActivityViewModel(
                         .build()
                 )
             integrityTokenResponse.addOnSuccessListener { response ->
-                sendTokenToLocal(response.token(), applicationContext)
+                sendTokenToLocal(response.token(), _resultRAW)
             }.addOnFailureListener { e ->
                 handleError(e)
             }
         } catch (e: Exception) {
             _result.postValue(DataResult.error("Exception: ${e.message}"))
         }
-    }
-
-
-    // TODO: Only on build internal testing can test
-    private fun sendTokenToLocal(token: String, applicationContext: Context) {
-        val base64OfEncodedDecryptionKey = BuildConfig.base64_of_encoded_decryption_key
-        val base64OfEncodedVerificationKey = BuildConfig.base64_of_encoded_verification_key
-
-        // base64OfEncodedDecryptionKey is provided through Play Console.
-        var decryptionKeyBytes: ByteArray =
-            Base64.decode(base64OfEncodedDecryptionKey, Base64.DEFAULT)
-
-        // Deserialized encryption (symmetric) key.
-        var decryptionKey: SecretKey = SecretKeySpec(
-            decryptionKeyBytes,
-            /* offset= */ 0,
-            decryptionKeyBytes.size,
-            "AES"
-        )
-
-        // base64OfEncodedVerificationKey is provided through Play Console.
-        var encodedVerificationKey: ByteArray =
-            Base64.decode(base64OfEncodedVerificationKey, Base64.DEFAULT)
-
-        // Deserialized verification (public) key.
-        var verificationKey: PublicKey = KeyFactory.getInstance("EC")
-            .generatePublic(X509EncodedKeySpec(encodedVerificationKey))
-
-        val jwe: JsonWebEncryption =
-            JsonWebStructure.fromCompactSerialization(token) as JsonWebEncryption
-        jwe.setKey(decryptionKey)
-
-        // This also decrypts the JWE token.
-        val compactJws: String = jwe.payload
-
-        val jws: JsonWebSignature =
-            JsonWebStructure.fromCompactSerialization(compactJws) as JsonWebSignature
-        jws.setKey(verificationKey)
-
-        // This also verifies the signature.
-        val payload: String = jws.getPayload()
-
-        val gson = Gson()
-        val piaResponse: PIAResponse = gson.fromJson(payload, PIAResponse::class.java)
-
-        Toast.makeText(
-            applicationContext,
-            "PIA Response: $piaResponse",
-            Toast.LENGTH_LONG
-        ).show()
-
-        _resultRAW.postValue(piaResponse.let {
-            DataResult.success(it)
-        })
     }
 }
--- app/src/main/java/com/kblack/demo_play_integrity_api/utils/Utils.kt
+++ app/src/main/java/com/kblack/demo_play_integrity_api/utils/Utils.kt
@@ -3,8 +3,21 @@ package com.kblack.demo_play_integrity_api.utils
 import android.util.Base64
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import com.google.gson.Gson
+import com.kblack.base.utils.DataResult
+import com.kblack.demo_play_integrity_api.BuildConfig
+import com.kblack.demo_play_integrity_api.model.PIAResponse
+import org.jose4j.jwe.JsonWebEncryption
+import org.jose4j.jws.JsonWebSignature
+import org.jose4j.jwx.JsonWebStructure
+import java.security.KeyFactory
+import java.security.PublicKey
 import java.security.SecureRandom
+import java.security.spec.X509EncodedKeySpec
+import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
 
 class Utils {
     companion object {
@@ -28,5 +41,62 @@ class Utils {
             random.nextBytes(bytes)
             return Base64.encodeToString(bytes, Base64.URL_SAFE or Base64.NO_WRAP)
         }
+
+
+        /**
+         * Sends the token to local for testing purposes.
+         * This function is used to decrypt and verify the integrity token.
+         *
+         * @param token The integrity token to be processed.
+         * @param _resultRAW MutableLiveData to post the result of the operation.
+         */
+
+        // TODO: Only on build internal testing can test
+        fun sendTokenToLocal(token: String, _resultRAW: MutableLiveData<DataResult<Any>?>) {
+            val base64OfEncodedDecryptionKey = BuildConfig.base64_of_encoded_decryption_key
+            val base64OfEncodedVerificationKey = BuildConfig.base64_of_encoded_verification_key
+
+            // base64OfEncodedDecryptionKey is provided through Play Console.
+            val decryptionKeyBytes: ByteArray =
+                Base64.decode(base64OfEncodedDecryptionKey, Base64.DEFAULT)
+
+            // Deserialized encryption (symmetric) key.
+            val decryptionKey: SecretKey = SecretKeySpec(
+                decryptionKeyBytes,
+                /* offset= */ 0,
+                decryptionKeyBytes.size,
+                "AES"
+            )
+
+            // base64OfEncodedVerificationKey is provided through Play Console.
+            val encodedVerificationKey: ByteArray =
+                Base64.decode(base64OfEncodedVerificationKey, Base64.DEFAULT)
+
+            // Deserialized verification (public) key.
+            val verificationKey: PublicKey = KeyFactory.getInstance("EC")
+                .generatePublic(X509EncodedKeySpec(encodedVerificationKey))
+
+            val jwe: JsonWebEncryption =
+                JsonWebStructure.fromCompactSerialization(token) as JsonWebEncryption
+            jwe.setKey(decryptionKey)
+
+            // This also decrypts the JWE token.
+            val compactJws: String = jwe.payload
+
+            val jws: JsonWebSignature =
+                JsonWebStructure.fromCompactSerialization(compactJws) as JsonWebSignature
+            jws.setKey(verificationKey)
+
+            // This also verifies the signature.
+            val payload: String = jws.getPayload()
+
+            val gson = Gson()
+            val piaResponse: PIAResponse = gson.fromJson(payload, PIAResponse::class.java)
+
+            _resultRAW.postValue(piaResponse.let {
+                DataResult.success(it)
+            })
+        }
+
     }
 }

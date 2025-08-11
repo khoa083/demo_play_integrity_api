808824684a4d94b3e47569cc39368ecd42f9a78c
Kblack (Solo dev) <65351458+khoa083@users.noreply.github.com>
2025-08-11 23:12:53 +0700

    upd res frame

--- app/src/main/java/com/kblack/demo_play_integrity_api/model/PIAFrame.kt
+++ app/src/main/java/com/kblack/demo_play_integrity_api/model/PIAFrame.kt
@@ -3,11 +3,11 @@ package com.kblack.demo_play_integrity_api.model
 import com.google.gson.annotations.SerializedName
 
 //@Deprecated("Use new request model instead")
-//data class PIAFrame<T>(
-//    val status: String,
-//    val verdict: Verdict<T>,
-//)
+data class PIAFrame<T>(
+    val status: String,
+    val verdict: Verdict<T>,
+)
 //@Deprecated("Use new request model instead")
-//data class Verdict<T>(
-//    @SerializedName("tokenPayloadExternal") val tokenPayloadExternal: T
-//)
+data class Verdict<T>(
+    @SerializedName("tokenPayloadExternal") val tokenPayloadExternal: T
+)
